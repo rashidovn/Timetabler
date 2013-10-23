c0ad4447b27b5b9ca9a13d6d5c53aac2297e9164
Blake Hawkins <Cokemonkey11@gmail.com>
2013-10-24 00:19:43 +0100

    oct242013

--- src/info/blakehawkins/timetabler/MainViewer.java
+++ src/info/blakehawkins/timetabler/MainViewer.java
@@ -1,13 +1,10 @@
 package info.blakehawkins.timetabler;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
 
-public class MainViewer extends Activity {
+public class MainViewer extends ActionBarActivity {
 
 	public final static String EXTRA_MESSAGE = "info.blakehawkins.timetabler.SENT_MESSAGE";
 
@@ -25,12 +22,12 @@ public class MainViewer extends Activity {
 	}
 
 	// ** Called when the send button is pressed */
-	public void sendMessage(View view) {
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
-		startActivity(intent);
-	}
+//	public void sendMessage(View view) {
+//		Intent intent = new Intent(this, DisplayMessageActivity.class);
+//		EditText editText = (EditText) findViewById(R.id.edit_message);
+//		String message = editText.getText().toString();
+//		intent.putExtra(EXTRA_MESSAGE, message);
+//		startActivity(intent);
+//	}
 
 }
