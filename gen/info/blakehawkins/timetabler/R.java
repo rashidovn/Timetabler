01fc64b23cf225a968b62a6ed1cbbcbe21dd072f
Blake Hawkins <Cokemonkey11@gmail.com>
2013-11-23 13:45:28 +0000

    Debugged Timetable.XML parser and now lists course acronyms currently chosen

--- gen/info/blakehawkins/timetabler/R.java
+++ gen/info/blakehawkins/timetabler/R.java
@@ -994,20 +994,15 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_refreshXML=0x7f050048;
-        public static final int action_selCourses=0x7f050047;
-        public static final int action_settings=0x7f05004a;
-        public static final int action_swapSem=0x7f050049;
+        public static final int action_refreshXML=0x7f05003d;
+        public static final int action_selCourses=0x7f05003c;
+        public static final int action_settings=0x7f05003f;
+        public static final int action_swapSem=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int course1=0x7f05003f;
-        public static final int course2=0x7f050040;
-        public static final int course3=0x7f050042;
-        public static final int course4=0x7f050045;
-        public static final int course5=0x7f050046;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1016,7 +1011,6 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int friday=0x7f050044;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1025,7 +1019,6 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
-        public static final int monday=0x7f05003d;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1042,7 +1035,6 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
-        public static final int semMessage=0x7f05003c;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1050,13 +1042,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int thursday=0x7f050043;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int tuesday=0x7f05003e;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int wednesday=0x7f050041;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
